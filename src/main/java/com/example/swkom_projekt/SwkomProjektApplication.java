51c84599041695c40a8d46eb8e7fdb53ed93c11e
soph28 <sophia.ehmayer@gmail.com>
2024-09-16 00:13:22 +0200

    Initial commit

---
+++ src/main/java/com/example/swkom_projekt/SwkomProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.swkom_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwkomProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwkomProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swkom_projekt/SwkomProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swkom_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwkomProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
