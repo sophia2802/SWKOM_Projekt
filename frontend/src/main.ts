43bbeb790f26ea15f32701bb85db8100693dfe67
soph28 <sophia.ehmayer@gmail.com>
2024-09-30 19:05:19 +0200

    implement cross origin

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
-
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  ...appConfig,
   providers: [
-    provideHttpClient(),
-    ...(appConfig.providers || []),
-  ],
-}).catch(err => console.error(err));
+    provideRouter(routes),
+    provideHttpClient(withFetch())
+  ]
+}).catch((err) =>
+  console.error(err)
+);
