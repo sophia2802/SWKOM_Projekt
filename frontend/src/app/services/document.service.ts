c5181f0668dc488bbfdca745f86ad51a42a0736e
soph28 <sophia.ehmayer@gmail.com>
2024-09-30 19:15:16 +0200

    add comment

--- frontend/src/app/services/document.service.ts
+++ frontend/src/app/services/document.service.ts
@@ -7,14 +7,14 @@ import { Observable } from 'rxjs';
 })
 export class DocumentService {
 
-  private documentUrl = 'http://localhost/api/documents';  // API URL für Dokumente
-  private _headers: HttpHeaders = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'}); // for requests with formdata
+  private documentUrl = 'http://localhost/api/documents';
+  private _headers: HttpHeaders = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
 
   constructor(private http: HttpClient) {}
 
   uploadDocument(file: File): Observable<any> {
     const formData: FormData = new FormData();
-    formData.append('file', file);  // Datei zur FormData hinzufügen
+    formData.append('file', file);
 
     return this.http.post<string>(this.documentUrl + '/upload', formData, { responseType: 'text' as 'json' });
   }
